2ea6b76979af7be7ef58d76cdb64876374f933d6
J.Alex <alqadianoKira@gmail.com>
2023-02-11 04:21:23 +0000

    src(components/Header) create the html of header

--- event-page/src/components/Header/index.tsx
+++ event-page/src/components/Header/index.tsx
@@ -1,6 +1,27 @@
 import { NavLink } from "react-router-dom";
 export const Header = () => {
   return (
-    <div>index</div>
+    <header>
+      <h1>LOGO</h1>
+        
+      <h1>| Events</h1>
+      <nav>
+        <ul>
+          <li>
+            <NavLink to="/">Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="/about">
+              About
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/previousevents">
+                Previous Events
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
   )
 }
