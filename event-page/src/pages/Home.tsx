3d738d32e91af63f0b5a298ac0a5aa735d62bdcb
J.Alex <alqadianoKira@gmail.com>
2023-02-11 04:14:14 +0000

    src(pages): create the Home page

---
+++ event-page/src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import { Header } from '../components/Header'
+
+function Home() {
+    return (
+        <div>
+            <Header />
+
+            <h1>VOCE ESTA NA HOME</h1>
+        </div>
+    )
+}
+
+export default Home
